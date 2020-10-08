cda2170a9bc789cf5ac5b589de375ceda3ca4351
renansdf <rnn.frts@gmail.com>
2020-10-07 22:14:37 -0300

    added user query and attach current user to abrigo

--- front/src/pages/Profile/index.tsx
+++ front/src/pages/Profile/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAbrigo } from '../../hooks/AbrigoHook';
+import api from '../../services/api';
 
 import Navbar from '../../components/Navbar';
 import TopMenu from '../../components/TopMenu';
@@ -7,19 +9,58 @@ import TopMenu from '../../components/TopMenu';
 import { Container } from './styles';
 import ProfileForm from '../../components/ProfileForm';
 import NavbarDesktop from '../../components/NavbarDesktop';
+import { IAbrigosData } from '../../components/AbrigoForm';
 
 interface IRouteParams {
   id: string;
 }
 
+export interface IUserData {
+  id: number;
+  nome: string;
+  idade: string;
+  profissao: string;
+}
+
 const Profile: React.FC = () => {
   const { id } = useParams<IRouteParams>();
+  const { hookAbrigo, setHookAbrigo } = useAbrigo();
+  const [user, setUser] = useState<IUserData>();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const handleAddProfissionalAbrigo = () => {
+    try {
+      if (user) {
+        api.put(`/abrigos/${hookAbrigo.id}`, {
+          ...hookAbrigo, profissionais: [...hookAbrigo.profissionais, { id: user.id, nome: user.nome }]
+        });
+        setHookAbrigo({} as IAbrigosData);
+        setIsVisible(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    api.get(`/users/${id}`).then(response => {
+      setUser(response.data);
+    });
+
+    if (hookAbrigo.id) {
+      setIsVisible(true);
+    }
+  }, []);
 
   return (
     <Container>
       <TopMenu />
 
-      <ProfileForm id={id} headingText="editar perfil" />
+      {isVisible && (
+        <button onClick={handleAddProfissionalAbrigo}>adicionar este usuário ao abrigo {hookAbrigo.nome}</button>
+      )}
+
+      <ProfileForm id={id} user={user} headingText="editar perfil" />
 
       <Navbar />
       <NavbarDesktop />
