df37ac3021bcfdc6aa5e6e0773d478cac6f92bf3
renansdf <rnn.frts@gmail.com>
2020-10-08 21:21:43 -0300

    replaced abrigo provider with the app provider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/Global';
 import Routes from './routes';
-import { AbrigoHookProvider } from './hooks/AbrigoHook';
+import AppProvider from './hooks/appProvider';
 
 function App() {
   return (
     <Router>
-      <AbrigoHookProvider>
+      <AppProvider>
         <Routes />
-      </AbrigoHookProvider>
+      </AppProvider>
       <GlobalStyle />
     </Router>
   );
