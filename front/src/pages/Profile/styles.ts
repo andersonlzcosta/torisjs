557ba1a9bb95f29db84dea3f58c61376fa0199dd
renansdf <rnn.frts@gmail.com>
2020-10-08 21:36:27 -0300

    styled new elements for warnings

--- front/src/pages/Profile/styles.ts
+++ front/src/pages/Profile/styles.ts
@@ -1,61 +1,55 @@
+import { darken } from 'polished';
 import styled from 'styled-components';
 import { DefaultContainer } from '../../styles/DefaultContainer';
-import { DefaultBackground } from '../../styles/DefaultBackground';
 
 export const Container = styled.div`
   ${DefaultContainer}
+  
+  button.floating{
+    width: 100%;
+    max-width: 500px;
+
+    letter-spacing: 1px;
+    font-size: 12px;
+    z-index: 12;
+    color: #002F67;
+    background: #D6E9FA;
+    box-shadow: 0px 15px 38px 0px #426fa340;
+
+    &:hover{
+      background: #bcdfff;
+    }
+  }
 `;
 
-export const DefaultContent = styled.div`
-  width: 90%;
+export const ButtonsContainer = styled.div`
+  flex-direction: row;
+  align-items: stretch;
   max-width: 500px;
-`;
+  margin-top: 20px;
 
-export const Content = styled.div`
-  ${DefaultBackground}
+  button{
+    background: #dc7835;
+    color: #fff;
+    font-size: 12px;
+    letter-spacing: 1px;
 
-  form{
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding: 0;
-
-    input{
-      background: none;
-      padding: 0px;
-      /* border: none;
-      border-bottom: 1px solid #0B4385; */
-      color: #0B4385;
-      border-radius: 0;
-      letter-spacing: 0px;
-      width: 100%;
-      font-weight: 300;
-      font-size: 17px;
-      margin: 4px 0 0;
+    &:hover{
+      background: ${darken(0.1, '#dc7835')};
     }
+  }
 
-    > div{
-      align-items: flex-start;
-      margin: 10px 0;
-    }
-
-    > div.full-width{
-      width: 100%;
-    }
-
-    > div.half-width{
-      width: 49%;
-    }
-
-    label{
-      font-weight: 900;
-      font-size: 13px;
-    }
-
-    .bigger{
-      font-weight: 300;
-      font-size: 27px;
+  button.voltar{
+    background: #D6E9FA;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+    
+    &:hover{
+      background: #bcdfff;
     }
   }
 `;
