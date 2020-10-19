9e4bd2978cf201e21d45c352a8cc837adca36fc0
Anderson <andersonlzcosta@gmail.com>
2020-10-19 10:41:37 -0300

    Routes adaptation to typeorm

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,22 +1,35 @@
 import { Router } from 'express';
 //import User from '../models/User';
+import { getCustomRepository } from "typeorm";
+
 import UsersRepository from '../repositories/UsersRepository';
+import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
-const usersRepository = new UsersRepository();
 
-usersRouter.get('/', (request, response) => {
-    const users = usersRepository.all();
+usersRouter.get('/', async (request, response) => {
+        const usersRepository = getCustomRepository(UsersRepository);
+        const users = await usersRepository.find();
 
     return response.json(users);
 });
 
-usersRouter.post('/', (request, response) => {
-    const { nome, idade, profissao } = request.body;
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { nome, idade, profissao } = request.body;
 
-    const user = usersRepository.create({nome, idade, profissao});
-    
-    return response.json(user);
+        const createUser = new CreateUserService();
+        
+        const user = await createUser.execute({
+            nome,
+            idade,
+            profissao
+        });
+        
+        return response.json(user);
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    }
 });
 
 export default usersRouter;
