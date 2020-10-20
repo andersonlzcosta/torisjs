2ef2fc817d3c87721c0c898893d25335349166e1
Anderson <andersonlzcosta@gmail.com>
2020-10-20 19:57:21 -0300

    Integration between server and database on docker-compose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import routes from './routes';
 
 import './database';
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(routes);
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hellowtheeere2e'});
-})
+  return response.json({ message: 'Hellowtheeere2e'});
+});
 
-app.listen(3333, () => {
-    console.log('Server started on port 3333');
-})
+app.listen(process.env.PORT || 2222, () => {
+  console.log('Server started on port 2222');
+});
