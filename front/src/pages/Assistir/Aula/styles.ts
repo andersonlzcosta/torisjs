9757157dfa400f0e4e4af02b1e7b694eaab55d4d
renansdf <rnn.frts@gmail.com>
2020-10-23 21:19:51 -0300

    modified the background and layout of the page

--- front/src/pages/Assistir/Aula/styles.ts
+++ front/src/pages/Assistir/Aula/styles.ts
@@ -4,9 +4,77 @@ import { DefaultContainer } from '../../../styles/DefaultContainer';
 
 export const Container = styled.div`
   ${DefaultContainer}
+  background-color: #fad552;
+  min-height: 100vh;
+  justify-content:center;
+  padding-top: 100px;
+  padding-bottom: 100px;
 `;
 
-export const AulaContent = styled.div`
-  ${DefaultBackground}
-  max-width: 600px;
+export const AulaContainer = styled.div`
+  width: 90%;
+  max-width: 900px;
+  width: 100%;
+
+  h1, h2{
+    z-index: 1;
+    margin-bottom: 5px;
+    max-width: 100%;
+  }
+
+  h1::before, h2::before{
+    background: #9fc1e0;
+  }
+
+  h2{
+    margin-top: 70px;
+    margin-bottom: 15px;
+  }
+
+  > div{
+    max-height: 500px;
+  }
+
+  span{
+    width: 100%;
+    padding-left: 40px;
+    font-size: 20px;
+    margin-bottom: 30px;
+  }
+
+  button.voltar{
+    box-shadow: 0px 10px 28px 0px #847a36;
+    letter-spacing: 1px;
+    font-size: 12px;
+    padding: 5px 15px;
+    width: auto;
+    align-self: flex-start;
+  }
+
+  button.proxima{
+    box-shadow: 0px 10px 28px 0px #847a36;
+    letter-spacing: 1px;
+    font-size: 15px;
+    padding: 10px 20px;
+    width: auto;
+    align-self: flex-start;
+  }
+`;
+
+export const AulaArquivos = styled.div`
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 100%;
+
+  button{
+    box-shadow: 0px 10px 28px 0px #847a36;
+    letter-spacing: 1px;
+    font-size: 13px;
+    padding: 9px 20px;
+    width: auto;
+  }
+
+  button + button{
+    margin-left: 10px;
+  }
 `;
