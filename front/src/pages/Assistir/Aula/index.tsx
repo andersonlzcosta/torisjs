284da496be4fa6c4c246c4fcb747eb4f1513fecb
renansdf <rnn.frts@gmail.com>
2020-10-23 21:18:34 -0300

    added ReactPlayer to handle youtube video embed

--- front/src/pages/Assistir/Aula/index.tsx
+++ front/src/pages/Assistir/Aula/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import api from '../../../services/api';
+
 import Navbar from '../../../components/Navbar';
 import NavbarDesktop from '../../../components/NavbarDesktop';
 import TopMenu from '../../../components/TopMenu';
-import api from '../../../services/api';
+import ReactPlayer from 'react-player';
 
-import { Container, AulaContent } from './styles';
+import { Container, AulaContainer, AulaArquivos } from './styles';
 
 interface IRouteParams {
   id: string;
@@ -22,6 +24,7 @@ interface IAula {
 const Aula: React.FC = () => {
   const { id } = useParams<IRouteParams>();
   const [aula, setAula] = useState<IAula>();
+  const { goBack } = useHistory();
 
   useEffect(() => {
     api.get(`/aulas/${id}`).then(response => {
@@ -34,12 +37,21 @@ const Aula: React.FC = () => {
       <TopMenu />
 
       {aula && (
-        <AulaContent>
+        <AulaContainer>
+          <button className="voltar" onClick={goBack}>Voltar</button>
           <h1>{aula.nome}</h1>
           <span>{aula.duracao}</span>
+          <ReactPlayer url={aula.video_url} width="100%" height="42vw" />
 
+          <h2>Arquivos</h2>
+          <AulaArquivos>
+            <button>Arquivos da aula em PDF</button>
+            <button>Arquivos da aula em CSV</button>
+          </AulaArquivos>
 
-        </AulaContent>
+          <h2>Próxima etapa</h2>
+          <button className="proxima">clique para assistir a próxima aula / pergunta</button>
+        </AulaContainer>
       )}
 
       <Navbar />
