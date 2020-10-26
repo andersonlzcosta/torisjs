a2462bc0e7c940fe9637130678c033b0bceb71d8
renansdf <rnn.frts@gmail.com>
2020-10-26 19:26:20 -0300

    changed findAll method and dropped unnecessary dependencies

--- server/src/modules/users/repositories/IUsersRepository.ts
+++ server/src/modules/users/repositories/IUsersRepository.ts
@@ -1,10 +1,9 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
-import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
-export default interface  IUsersRepository {
+export default interface IUsersRepository {
     findById(id: string): Promise<User | undefined>;
-    findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
+    findAll(): Promise<User[]>;
     findByEmail(email: string): Promise<User | undefined>;
     create(data: ICreateUserDTO): Promise<User>;
     save(user: User): Promise<User>;
