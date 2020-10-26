85d59c9207753968263adcd9e7296413ffdf433b
Anderson <andersonlzcosta@gmail.com>
2020-10-26 12:07:14 -0300

    Users model  and Repository becomes a part of typeorm, located on each entity infra folder. Model are now refered as Entity!

--- server/src/repositories/UsersRepository.ts
+++
@@ -1,8 +0,0 @@
-import User from '../models/User';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(User)
-class UsersRepository extends Repository<User> {
-}
-
-export default UsersRepository;
