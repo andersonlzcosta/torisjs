3e0545620e34f6f632f6a3124300fe482e9c2320
renansdf <rnn.frts@gmail.com>
2020-10-26 21:15:40 -0300

    create default styles for popup

--- front/src/components/Popup/styles.ts
+++ front/src/components/Popup/styles.ts
@@ -1,9 +1,57 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
 
 interface IContainerProps {
   isVisible: boolean;
 }
 
 export const Container = styled.div<IContainerProps>`
+  display: none;
+
+  ${props => props.isVisible && css`
+    display: flex;
+  `}
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  z-index: 22;
+`;
+
+export const Content = styled.div`
+  width: 90%;
+  max-width: 500px;
   
+  div{
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  p{
+    font-size: 35px;
+    text-align: center;
+  }
+
+  button{
+    width: 48%;
+    background: #dd4c37;
+
+    &:hover{
+      background: ${shade(.2, '#dd4c37')};
+    }
+  }
+  
+  button:last-child{
+    background: green;
+
+    &:hover{
+      background: ${shade(.2, 'green')};
+    }
+  }
 `;
