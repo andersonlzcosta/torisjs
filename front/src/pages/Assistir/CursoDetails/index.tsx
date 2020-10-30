fc98fb7c695d21477dfcdcb64ba9921bcee90a29
renansdf <rnn.frts@gmail.com>
2020-10-30 15:19:36 -0300

    updated component to reflect new cursos data model

--- front/src/pages/Assistir/CursoDetails/index.tsx
+++ front/src/pages/Assistir/CursoDetails/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import api from '../../../services/api';
 
@@ -6,7 +6,7 @@ import TopMenu from '../../../components/TopMenu';
 import NavbarDesktop from '../../../components/NavbarDesktop';
 import Navbar from '../../../components/Navbar';
 
-import { Container, CursoContent, Aula, Popup } from './styles';
+import { Container, CursoContent, Modulo, Aula, Popup } from './styles';
 import { useToast } from '../../../hooks/toast';
 
 interface IRouteParams {
@@ -18,20 +18,26 @@ export interface IAulasData {
   nome: string;
   video_url: string;
   assistida: boolean;
-  duracao: string;
+  duracao: number;
+}
+
+interface IModuloData {
+  id: number;
+  nome: string;
+  aulas: IAulasData[];
 }
 
 export interface ICursoData {
   id: number;
   nome: string;
   descricao: string;
-  aulas: IAulasData[];
+  modulos: IModuloData[];
 }
 
 const CursoDetails: React.FC = () => {
   const { id } = useParams<IRouteParams>();
   const [curso, setCurso] = useState<ICursoData>();
-  const [isSubscribed, setIsSubscribed] = useState<boolean>(true);
+  const [isSubscribed, setIsSubscribed] = useState<boolean>(false);
   const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
 
   const { addToast } = useToast();
@@ -45,6 +51,22 @@ const CursoDetails: React.FC = () => {
       message: 'você tem 30 dias para terminar este curso.'
     });
   }
+
+  const aulasTotal = useMemo(() => {
+    if (curso) {
+      const aulasPorModulo = curso.modulos.map(modulo => modulo.aulas.length);
+      return aulasPorModulo.reduce((accumulator, currentValue) => accumulator + currentValue);
+    }
+  }, [curso]);
+
+  const duracaoTotal = useMemo(() => {
+    if (curso) {
+      const todasAulasArray = curso.modulos.map(modulo => {
+        return modulo.aulas.reduce((accumulator, currentValue) => accumulator + currentValue.duracao, 0);
+      });
+      return todasAulasArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+    }
+  }, [curso]);
 
   useEffect(() => {
     api.get(`/cursos/${id}`).then(response => {
@@ -60,8 +82,9 @@ const CursoDetails: React.FC = () => {
         <CursoContent>
           <h3>{curso.nome}</h3>
           <p>{curso.descricao}</p>
-          <span>total de aulas: {curso.aulas.length}</span>
-          <span>duração total: {curso.aulas.length}</span>
+          <span>{curso.modulos.length} módulo(s)</span>
+          <span>{aulasTotal} aula(s)</span>
+          <span>duração total de {duracaoTotal} minutos</span>
 
           <button onClick={() => setIsPopupVisible(true)}>quero me inscrever</button>
         </CursoContent>
@@ -71,14 +94,22 @@ const CursoDetails: React.FC = () => {
         <CursoContent>
           <h3>{curso.nome}</h3>
           <p>{curso.descricao}</p>
-          <p>duração total: {curso.aulas.length}</p>
-          {curso.aulas.map(aula => (
-            <Aula hasBeenWatched={aula.assistida}>
-              <Link to={`/aula/${aula.id}`} key={aula.id}>
-                <h3>{aula.nome}</h3>
-              </Link>
-              <span>{aula.duracao}</span>
-            </Aula>
+          <span>{curso.modulos.length} módulo(s)</span>
+          <span>{aulasTotal} aula(s)</span>
+          <span>duração total de {duracaoTotal} minutos</span>
+          {curso.modulos.map(modulo => (
+            <Modulo key={modulo.id}>
+              <h4>{modulo.nome}</h4>
+              {modulo.aulas.map(aula => (
+                <Aula hasBeenWatched={aula.assistida}>
+                  {/* <Link to={`/aula/${aula.id}`} key={aula.id}> */}
+                  <Link to={`/aula/933`} key={aula.id}>
+                    <h4>{aula.nome}</h4>
+                  </Link>
+                  <span>{aula.duracao} minutos</span>
+                </Aula>
+              ))}
+            </Modulo>
           ))}
         </CursoContent>
       )}
