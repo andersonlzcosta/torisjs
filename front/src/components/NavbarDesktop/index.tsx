d09e8c1dce685b56e5c46c1e4c630950d6b0e806
renansdf <rnn.frts@gmail.com>
2020-10-30 15:17:54 -0300

    commented routes for estatisticas of every view

--- front/src/components/NavbarDesktop/index.tsx
+++ front/src/components/NavbarDesktop/index.tsx
@@ -12,18 +12,18 @@ const NavbarDesktop: React.FC = () => {
         <Link to="/dashboard">dashboard</Link>
 
         <label>abrigos</label>
-        <Link to="/abrigos/estatisticas">estatísticas</Link>
+        {/* <Link to="/abrigos/estatisticas">estatísticas</Link> */}
         <Link to="/abrigos/todos">ver todos</Link>
         <Link to="/abrigos/novo">criar novo</Link>
 
         <label>profissionais</label>
-        <Link to="/profissionais/estatisticas">estatísticas</Link>
+        {/* <Link to="/profissionais/estatisticas">estatísticas</Link> */}
         <Link to="/profissionais/todos">ver todos</Link>
         <Link to="/profissionais/novo">criar novo</Link>
 
         <label>cursos</label>
-        <Link to="/cursos/estatisticas">estatísticas</Link>
-        <Link to="/cursos/todos">editar todos</Link>
+        {/* <Link to="/cursos/estatisticas">estatísticas</Link> */}
+        <Link to="/cursos/todos">ver todos</Link>
         <Link to="/cursos/novo">criar novo</Link>
         <Link to="/assistir">assistir cursos</Link>
 
