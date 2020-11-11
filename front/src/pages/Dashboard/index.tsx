2d72e27dc09aa283442996acadb21d9bf8b728ec
renansdf <rnn.frts@gmail.com>
2020-11-11 19:08:29 -0300

    added notifications component to show list of active notifications of current user

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import Navbar from '../../components/Navbar';
-import TopMenu from '../../components/TopMenu';
-import { Container, UserList, User, AbrigosList, Abrigo } from './styles';
-
-import Perfil from '../../images/perfil.jpg'
-import Plus from '../../images/plus.svg'
-import api from '../../services/api';
 import NavbarDesktop from '../../components/NavbarDesktop';
+import TopMenu from '../../components/TopMenu';
+
+import { Container, NotificationContainer, UserList, User, AbrigosList, Abrigo } from './styles';
 import { Link } from 'react-router-dom';
 import { IAbrigosData } from '../../components/AbrigoForm';
+
+import Perfil from '../../images/perfil.jpg'
+import { FiMinusCircle } from 'react-icons/fi';
+import { useToast } from '../../hooks/toast';
 
 interface IProfissionaisData {
   id: number;
@@ -18,9 +21,53 @@ interface IProfissionaisData {
   profissao: string;
 }
 
+interface INotificacao {
+  id: number;
+  type: string;
+  message: string;
+}
+
+interface IUser {
+  id: number;
+  nome: string;
+  idade: string;
+  profissao: string;
+  archivedNotifications: number[];
+}
+
 const Dashboard: React.FC = () => {
   const [profissionais, setProfissionais] = useState<IProfissionaisData[]>();
   const [abrigos, setAbrigos] = useState<IAbrigosData[]>();
+  const [activeNotifications, setActiveNotifications] = useState<INotificacao[]>();
+  const [archivedNotifications, setArchivedNotifications] = useState<number[]>();
+  const [thisUser, setThisUser] = useState<IUser>();
+
+  const { user } = useAuth();
+  const { addToast } = useToast();
+
+  const handleDelete = useCallback((notificationId: number) => {
+    if (activeNotifications && archivedNotifications && thisUser) {
+      const updatedNotifications = activeNotifications.filter(notification => notification.id !== notificationId);
+
+      setActiveNotifications(updatedNotifications);
+
+      const updatedArchive = [...archivedNotifications, notificationId]
+      setArchivedNotifications(updatedArchive);
+
+      api.put(`/users/${user.id}`, {
+        nome: thisUser.nome,
+        idade: thisUser.idade,
+        profissao: thisUser.profissao,
+        archivedNotifications: updatedArchive
+      });
+
+      addToast({
+        title: "Notificação arquivada!",
+        type: "success"
+
+      });
+    }
+  }, [activeNotifications, archivedNotifications, thisUser]);
 
   useEffect(() => {
     api.get('/users?_limit=2').then(response => {
@@ -31,13 +78,32 @@ const Dashboard: React.FC = () => {
       setAbrigos(response.data);
     });
 
-  }, [setProfissionais]);
-
+    Promise.all([
+      api.get('/notifications'),
+      api.get(`/users/${user.id}`)
+    ]).then(responses => {
+      const todasNotificacoes: INotificacao[] = responses[0].data;
+      const userData: IUser = responses[1].data;
+      const updatedActiveNotifications = todasNotificacoes.filter(notificacao => !userData.archivedNotifications.includes(notificacao.id));
+      setActiveNotifications(updatedActiveNotifications);
+      setArchivedNotifications(userData.archivedNotifications);
+      setThisUser(userData);
+    });
+  }, [user]);
 
   return (
     <Container>
       <TopMenu />
       <UserList>
+        <h2>notificações recentes</h2>
+
+        {activeNotifications && activeNotifications.map(notification => (
+          <NotificationContainer key={notification.id} type={notification.type}>
+            <p>{notification.message}</p>
+            <button onClick={() => handleDelete(notification.id)}><FiMinusCircle size={20} /></button>
+          </NotificationContainer>
+        ))}
+
         <h2>últimos profissionais cadastrados</h2>
 
         {profissionais && profissionais.map(profissional => (
