491c301d591229dee3353bfa00c16ac7496fa031
renansdf <rnn.frts@gmail.com>
2020-11-11 19:07:42 -0300

    added auth context provider

--- front/src/hooks/appProvider.tsx
+++ front/src/hooks/appProvider.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { AuthProvider } from './auth';
 import { AbrigoHookProvider } from '../hooks/AbrigoHook';
 import { ToastProvider } from '../hooks/toast';
 
 const AppProvider: React.FC = ({ children }) => (
-  <AbrigoHookProvider>
-    <ToastProvider>{children}</ToastProvider>
-  </AbrigoHookProvider>
+  <AuthProvider>
+    <AbrigoHookProvider>
+      <ToastProvider>{children}</ToastProvider>
+    </AbrigoHookProvider>
+  </AuthProvider>
 )
 
 export default AppProvider;
