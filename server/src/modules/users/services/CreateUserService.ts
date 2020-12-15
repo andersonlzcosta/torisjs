2b3ba4f05dcab1eab2d66ca878cf9f4a72ccf04d
Anderson <andersonlzcosta@gmail.com>
2020-12-15 05:52:01 -0300

    Added encryption to users password

--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import User from "../infra/typeorm/entities/User";
 import UsersRepository from "../infra/typeorm/repositories/UsersRepository";
@@ -15,12 +16,14 @@ class CreateUserService {
     public async execute({ nome, email, idade, profissao, password, abrigoId }: Request): Promise<User | undefined> {
         const usersRepository = getCustomRepository(UsersRepository);
 
+        const hashedPassword = await hash(password, 8);
+
         const user = await usersRepository.create({
             nome,
             email,
             idade,
             profissao,
-            password,
+            password: hashedPassword,
             abrigoId
         });
 
