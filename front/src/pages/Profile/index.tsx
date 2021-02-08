35d7370984bfc0d6ed99e23e44ba24554755751f
renansdf <rnn.frts@gmail.com>
2021-02-08 18:54:10 -0300

    isolated queries and added new fields to form

--- front/src/pages/Profile/index.tsx
+++ front/src/pages/Profile/index.tsx
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
 import { useAbrigo } from '../../hooks/AbrigoHook';
-import api from '../../services/api';
+import { useToast } from '../../hooks/toast';
 
+import ProfileForm from '../../components/ProfileForm';
+import NavbarDesktop from '../../components/NavbarDesktop';
+import { IAbrigosData } from '../../components/AbrigoForm';
 import Navbar from '../../components/Navbar';
 import TopMenu from '../../components/TopMenu';
 import Arrow from '../../images/arrow.svg';
 
 import { Container, ButtonsContainer } from './styles';
-import ProfileForm from '../../components/ProfileForm';
-import NavbarDesktop from '../../components/NavbarDesktop';
-import { IAbrigosData } from '../../components/AbrigoForm';
-import { useToast } from '../../hooks/toast';
+import { VER_USUARIO } from './apolloQueries';
+
 
 interface IRouteParams {
   id: string;
 }
 
-export interface IUserData {
-  id: number;
+interface IUserData {
+  id: string;
+  email: string;
+  password: string;
   nome: string;
-  idade: string;
+  emailAlternativo: string;
+  nascimento: any;
+  cargo: string;
+  telefone1: string;
+  telefone2: string;
   profissao: string;
+  abrigoId: string;
+}
+
+interface IGETQueryResponse {
+  verUsuario: {
+    user: IUserData;
+  }
 }
 
 const Profile: React.FC = () => {
@@ -32,47 +47,53 @@ const Profile: React.FC = () => {
   const history = useHistory();
   const { addToast } = useToast();
 
-  const handleAddProfissionalAbrigo = () => {
-    try {
-      if (user) {
-        const abrigoId = hookAbrigo.id;
-        api.put(`/abrigos/${hookAbrigo.id}`, {
-          ...hookAbrigo, profissionais: [...hookAbrigo.profissionais, { id: user.id, nome: user.nome }]
-        });
-        setHookAbrigo({} as IAbrigosData);
-        setIsVisible(false);
-        addToast({
-          type: 'success',
-          title: 'Profissional adicionado!',
-          message: 'informações do abrigo foram atualizadas.',
-        });
-        history.push(`/abrigo/${abrigoId}`);
-      }
-    } catch (err) {
-      console.log(err);
-      addToast({
-        type: 'error',
-        title: 'Erro ao adicionar',
-        message: 'tente novamente ou entre em contato com suporte.',
+  useQuery<IGETQueryResponse>(VER_USUARIO, {
+    variables: { id: id },
+    onCompleted(data) {
+      const nascimento = new Date(data.verUsuario.user.nascimento);
+      setUser({
+        ...data.verUsuario.user,
+        nascimento: nascimento
       });
-    }
+    },
+    onError() { addToast({ title: "erro ao carregar usuário" }) }
+  });
+
+  const handleAddProfissionalAbrigo = () => {
+    // try {
+    //   if (user) {
+    //     const abrigoId = hookAbrigo.id;
+    //     api.put(`/abrigos/${hookAbrigo.id}`, {
+    //       ...hookAbrigo, profissionais: [...hookAbrigo.profissionais, { id: user.id, nome: user.nome }]
+    //     });
+    //     setHookAbrigo({} as IAbrigosData);
+    //     setIsVisible(false);
+    //     addToast({
+    //       type: 'success',
+    //       title: 'Profissional adicionado!',
+    //       message: 'informações do abrigo foram atualizadas.',
+    //     });
+    //     history.push(`/abrigo/${abrigoId}`);
+    //   }
+    // } catch (err) {
+    //   console.log(err);
+    //   addToast({
+    //     type: 'error',
+    //     title: 'Erro ao adicionar',
+    //     message: 'tente novamente ou entre em contato com suporte.',
+    //   });
+    // }
   }
 
   useEffect(() => {
-    api.get(`/users/${id}`).then(response => {
-      setUser(response.data);
-    });
-
-    if (hookAbrigo.id) {
-      setIsVisible(true);
-    }
-  }, []);
+    hookAbrigo.id && setIsVisible(true);
+  }, [hookAbrigo]);
 
   return (
     <Container>
       <TopMenu />
 
-      <ProfileForm user={user} headingText="editar perfil" />
+      <ProfileForm inheritedUser={user} headingText="editar perfil" />
 
       {isVisible && (
         <ButtonsContainer>
