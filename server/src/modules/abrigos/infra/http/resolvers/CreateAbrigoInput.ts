20eaf7876f538f6d702cd204873de18dfc91515d
Anderson <andersonlzcosta@gmail.com>
2021-02-11 07:49:04 -0300

    Emancipated input types of abrigo resolver as well as nullifylability to fields and considered dependency injection on abrigos mutations

---
+++ server/src/modules/abrigos/infra/http/resolvers/CreateAbrigoInput.ts
@@ -0,0 +1,39 @@
+import { InputType, Field } from "type-graphql";
+
+@InputType()
+export class CriarAbrigoInput {
+
+    @Field()
+    nome: string;
+    @Field({ nullable: true })
+    telefone1: string;
+    @Field({ nullable: true })
+    telefone2: string;
+    @Field({ nullable: true })
+    email1: string;
+    @Field({ nullable: true })
+    email2: string;
+    @Field({ nullable: true })
+    endereco: string;
+    @Field({ nullable: true })
+    bairro: string;
+    @Field({ nullable: true })
+    cidade: string;
+    @Field({ nullable: true })
+    estado: string;
+    @Field({ nullable: true })
+    classificacao: string;
+    @Field({ nullable: true })
+    capacidade: string;
+    @Field({ nullable: true })
+    faixaEtaria: string;
+    @Field({ nullable: true })
+    lgbt: boolean;
+    @Field({ nullable: true })
+    genero: string;
+    @Field({ nullable: true })
+    pcd: boolean;
+    @Field({ nullable: true })
+    observacao: string;
+
+}
