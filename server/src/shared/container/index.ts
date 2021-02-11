9dc8d0730f187ff3ecd7c5030c2e8f28fd3963c7
Anderson <andersonlzcosta@gmail.com>
2021-02-11 06:57:15 -0300

    Created Notificacao repository, repository interface, DTOs and registered it at depedency injection

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -5,4 +5,8 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import INotificacoesRepository from '@modules/notificacoes/repositories/INotificacoesRepository';
+import NotificacoesRepository from '@modules/notificacoes/infra/typeorm/repositories/NotificacoesRepository';
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<INotificacoesRepository>('NotificacoesRepository', NotificacoesRepository);
