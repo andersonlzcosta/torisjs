6ef7e9f9ec010f7210fc80533bd1685a25169977
renansdf <rnn.frts@gmail.com>
2021-02-16 00:28:10 -0300

    refactored pergunta component to handle update functions and isolated queries

---
+++ front/src/pages/Pergunta/apolloQueries.ts
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client';
+
+
+export const GET_PERGUNTA_BY_ID = gql`
+query getPerguntaById($id: String!){
+  verForumPergunta(id: $id){
+    pergunta{
+      id,
+      titulo,
+      corpo,
+      foiResolvido,
+      createdAt,
+      respostas{
+        id,
+        corpo,
+        createdAt
+      }
+    }
+  }
+}`;
+
+export const DELETE_PERGUNTA_BY_ID = gql`
+mutation deletarPerguntaById($id: String!){
+  deletarForumPergunta(id: $id)
+}`;
+
+export const CREATE_RESPOSTA = gql`
+mutation createResposta($corpo: String!, $perguntaId: String!){
+  criarForumResposta(options: {
+    corpo: $corpo
+    perguntaId: $perguntaId
+  }){
+    resposta{
+      id
+      corpo
+    }
+  }
+}`;
+
+export const DELETE_RESPOSTA_BY_ID = gql`
+mutation deletarResposta($id: String!){
+  deletarForumResposta(id: $id)
+}`;
