a6db4f01e2c7540631bd65166c29581a45438fe1
renansdf <rnn.frts@gmail.com>
2021-02-16 00:28:32 -0300

    created new user register page

---
+++ front/src/pages/SignUp/index.tsx
@@ -0,0 +1,104 @@
+import React, { useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useMutation } from '@apollo/client';
+
+import { CREATE_USER } from './apolloQueries';
+import getValidationErrors from '../../utils/getValidationErrors';
+import { useToast } from '../../hooks/toast';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container } from './styles';
+
+interface credentialsData {
+  nome: string;
+  email: string;
+  pass: string;
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
+
+  const { addToast } = useToast();
+
+  const [CreateUser] = useMutation(CREATE_USER, {
+    onCompleted() {
+      addToast({
+        title: "Usuário criado!",
+        type: "success"
+      });
+      history.push('/');
+    },
+    onError() {
+      addToast({
+        title: "Erro ao criar usuário",
+        type: "error"
+      });
+    }
+  });
+
+  const handleSubmit = useCallback(async (formData: credentialsData) => {
+    try {
+      if (!formRef.current) {
+        throw new Error('formRef invalid');
+      }
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        nome: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail é obrigatório').email('Use um e-mail válido'),
+        pass: Yup.string().required('Senha Obrigatória'),
+      });
+
+      await schema.validate(formData, {
+        abortEarly: false,
+      });
+
+      CreateUser({
+        variables: {
+          nome: formData.nome,
+          email: formData.email,
+          pass: formData.pass
+        }
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        if (!formRef.current) {
+          throw new Error('formRef invalid');
+        }
+        formRef.current.setErrors(errors);
+        return
+      }
+
+      if (err instanceof Error) {
+        addToast({
+          title: err.message,
+          message: 'tente novamente',
+          type: 'error'
+        });
+      }
+
+    }
+  }, []);
+
+  return (
+    <Container style={{ maxWidth: 400 }}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Cadastre-se</h1>
+        <Input name="nome" placeholder="Nome" />
+        <Input name="email" placeholder="E-mail" type="email" />
+        <Input name="pass" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
+      </Form>
+
+      <Link to="/">Voltar ao login</Link>
+    </Container>
+  );
+}
+
+export default SignUp;
---
+++ front/src/pages/SignUp/styles.ts
@@ -0,0 +1,66 @@
+import { lighten } from 'polished';
+import styled from 'styled-components';
+import { DefaultBackground } from '../../styles/DefaultBackground';
+
+export const Container = styled.div`
+  ${DefaultBackground}
+  width: 90%;
+  max-width: 450px;
+
+  h1{
+    text-align: center;
+    font-size: 40px;
+    padding: 0px;
+    margin: 0px 0px 20px 0px;
+    font-weight: 400;
+    text-transform: lowercase;
+  }
+
+  > a{
+    width: 100%;
+    max-width: 250px;
+    text-align: center;
+    font-weight: 200;
+    font-size: 14px;
+    background: #D6E9FA;
+    color: #002F67;
+    border-radius: 30px;
+    padding: 8px 15px;
+    margin-top: 30px;
+    text-decoration: none;
+    transition: background .4s;
+
+    &:hover{
+      background: #bcdfff;
+    }
+  }
+
+  form{
+    width: 100%;
+  }
+
+  form a{
+    width: 100%;
+    border-radius: 30px;
+    border: none;
+    color: #fff;
+    letter-spacing: 4px;
+    font-size: 21px;
+    padding: 13px 35px;
+    text-align: center;
+    background: #5D3077;
+    box-shadow: 0px 10px 28px 0px #AD9DCD;
+    transition: background .4s;
+    cursor: pointer;
+
+    margin-top: 15px;
+    text-decoration: none;
+
+    &:focus{
+      outline: none;
+    }
+    &:hover{
+      background: ${lighten(0.2, '#5D3077')};
+    }
+  }
+`;
