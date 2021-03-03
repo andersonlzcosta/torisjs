7b060bfec6ea396d609ce237ec4eeeca8d5cda1a
Anderson <andersonlzcosta@gmail.com>
2021-03-03 09:40:54 -0300

    Registered Cursos repository to tsyringe

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -11,6 +11,25 @@ import AbrigosRepository from '@modules/abrigos/infra/typeorm/repositories/Abrig
 import INotificacoesRepository from '@modules/notificacoes/repositories/INotificacoesRepository';
 import NotificacoesRepository from '@modules/notificacoes/infra/typeorm/repositories/NotificacoesRepository';
 
+import ICursosRepository from '@modules/cursos/repositories/ICursosRepository';
+import CursosRepository from '@modules/cursos/infra/typeorm/repositories/CursosRepository';
+
+import IModulosRepository from '@modules/cursos/repositories/IModulosRepository';
+import ModulosRepository from '@modules/cursos/infra/typeorm/repositories/ModulosRepository';
+
+import IModulosPerguntasRepository from '@modules/cursos/repositories/IPerguntasRepository';
+import ModulosPerguntasRepository from '@modules/cursos/infra/typeorm/repositories/PerguntasRepository';
+
+import IModulosAulasRepository from '@modules/cursos/repositories/IAulasRepository';
+import ModulosAulasRepository from '@modules/cursos/infra/typeorm/repositories/AulasRepository';
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+
 container.registerSingleton<IAbrigosRepository>('AbrigosRepository', AbrigosRepository);
+
 container.registerSingleton<INotificacoesRepository>('NotificacoesRepository', NotificacoesRepository);
+
+container.registerSingleton<ICursosRepository>('CursosRepository', CursosRepository);
+container.registerSingleton<IModulosRepository>('ModulosRepository', ModulosRepository);
+container.registerSingleton<IModulosPerguntasRepository>('ModulosPerguntasRepository', ModulosPerguntasRepository);
+container.registerSingleton<IModulosAulasRepository>('ModulosAulasRepository', ModulosAulasRepository);
